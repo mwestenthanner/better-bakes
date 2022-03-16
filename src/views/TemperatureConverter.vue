e4ea4a7eef2bf3f46714531ddacb1bb0537c5796
Marianne Westenthanner <marianne@westenthanner.de>
2022-03-16 16:27:50 +0100

    Temperature converter logic

--- src/views/TemperatureConverter.vue
+++ src/views/TemperatureConverter.vue
@@ -4,15 +4,13 @@
         <h2>Temperature Converter</h2>
 
         <label for="celsius">Celsius</label>
-        <input id="celsius" type="number" v-model="celsius" />
+        <input id="celsius" type="number" v-model="celsius" @input="emptyInputs('celsius')"/>
 
         <label for="fahrenheit">Fahrenheit</label>
-        <input id="fahrenheit" type="number" />
+        <input id="fahrenheit" type="number" v-model="fahrenheit" @input="emptyInputs('fahrenheit')" />
 
         <button @click="calculateTemperature()">Convert</button>
 
-        <p>{{ celsius }}</p>
-        
     </main>
 </template>
 
@@ -22,19 +20,42 @@ import { defineComponent, ref } from 'vue';
 export default defineComponent({
     setup() {
 
-        let celsius = ref(null);
-        let fahrenheit = ref(null);
+        let celsius = ref();
+        let fahrenheit = ref();
 
         function calculateTemperature() {
 
-            console.log(celsius);
+            if (isNaN(celsius.value) && !isNaN(fahrenheit.value)) {
+
+                console.log(celsius.value);
+
+                celsius.value = (fahrenheit.value - 32) * 5/9;
+
+            } else if (!isNaN(celsius.value) && isNaN(fahrenheit.value)) {
+                
+                fahrenheit.value = celsius.value * 1.8 + 32
+
+            } 
+
+        }
+
+        function emptyInputs(input: string) {
+
+            if (input == 'celsius') {
+                fahrenheit.value = undefined;
+            }
+
+            if (input == 'fahrenheit') {
+                celsius.value = undefined;
+            }
 
         }
 
         return {
             celsius,
             fahrenheit,
-            calculateTemperature
+            calculateTemperature,
+            emptyInputs
         }
 
     }
